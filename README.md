db4f931702387e714c7496084a952fc51b8a45da
Alexander Dorofeev <aka.spin@gmail.com>
2012-06-18 18:50:20 +0600

    v0.3. Refactoring, Touch detection, docs. Tested, ready for production.
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,260 @@
 # Pan plugin for jQuery
 
-This is another bicycle for horizontal panning `div` contents. See `test.html`
-for details. 
+Horisontal panning for jQuery with touch fallback. 
+
+## Benefits and drawbacks
+
+* `+` No need to provide sizes, dimenisions and other.
+* `+` Minimal intervention in styles (Can be completelly disabled).
+* `+` Sweep support (Can be disabled). 
+* `+` Support to display right and left brakes (Can be disabled).
+* `+` Automatic disabling on touch devices (Overridable).
+* `+` Wide compatibility.
+
+* `-` Hardware acceleration depends on jQuery. 
+
+## Compatibility
+
+Most of modern and old browsers. Currently tested on Chrome, MSIE 7+ and 
+FF 3.6+, Chrome for Android, Android Browser (2.1 and 4.0.4).
+
+## Dependencies
+
+This plugin depend on [jQuery](http://jquery.com/) and optional 
+[jQuery Easing](http://gsgd.co.uk/sandbox/jquery/easing/).
+
+## Basic usage
+
+Very basic setup of pane is two nested `div`s (or anything that you need).
+
+    <!-- Wrapper -->
+    <div class='wrap'>
+    
+        <!-- And our hero -->
+        <div class='pan'>
+            My very long content...
+        </div>
+    </div>
+    
+... some css:
+
+    .wrap {
+      width: 90%;             /* Make it ellastic */
+      }
+      
+    .pan {
+      white-space: nowrap;  /* Place all in ribbon */
+      overflow: auto;       /* Good for touch devices and automatic 
+                               fallback for non-js browsers. */       
+      }
+      
+... and let's rock
+
+    $('.pan').panner();
+    
+## Touch devices
+
+By default `panner` kills himself on touch devices. This behaviour can be 
+overriden by option `touch`. `touch` is simple `function` that returns `true`
+if device supports touch events.
+
+    $(...).panner({ 
+        touch: function() {
+            return 'ontouchstart' in document.documentElement;
+            }
+    });
+    
+Instead this you can provide own `function` or boolean value. Be careful. 
+`panner` not supports touch events.
+
+## Container CSS
+
+To correctly display scrollable container `panner` automatically sets 
+`overflow: hidden` on it for non-touch devices. You can provide own styles by
+`css` option.
+
+    $(...).panner({ 
+        css: {'overflow': 'hidden'}
+    });
+
+## Brakes
+
+If you dig basic usage, you might notice that when ribbon (`.pan`) came up
+against towards right or left, one of the styles (`brake-left` or 
+`brake-right`) for half a second was added to the wrapper (`.wrap`). This can 
+be used to display brakes. Just small corrections to CSS.
+
+    .wrap {
+      position: relative;     /* Needed to correct display brakes */
+      width: 90%;             /* Make it ellastic */
+      }
+      
+    /* Brake styles. Just display empty rectangle on needed side */
+    .brake-left:after,
+    .brake-right:after {
+      content: "";
+      position: absolute;
+      height: 100%;
+      top: 0px;
+      width: 3px;
+      background-color: orange;
+      }
+    /* Show right brake on ... right */
+    .brake-right:after {
+      right: 0px;
+      }
+
+All behavior of brakes controlled by `brake` option.
+
+    $(...).panner({ 
+        brake: {
+            left : 'brake-left',
+            right : 'brake-right',
+            timeout : 500
+        }
+    });
+
+### Disabling
+
+Instead display brakes, you can completelly disable them. Just set `brake` 
+option to `false`.
+
+    $(...).panner({ 
+        brake: false
+    });
+
+Of course, in this case wrapper (`.wrap`) is not needed and can be purged 
+from layout.
+
+### Custom target
+
+By default, `panner` use parent element (`$('el').parent()`) to display 
+brakes. This good for most cases. To use different, set `brake.what` option.
+
+    $(...).panner({ 
+        brake: {
+            what: $('#my-brakes')
+        }
+    });
+
+### Timeout
+ 
+ By default, brakes are shown for half a second. To set other timeout use 
+ corresponding option. 
+ 
+    $(...).panner({ 
+        brake: {
+            timeout : 500
+        }
+    });
+
+### Styles
+
+To use other styles for display brakes, you can use options `brake.left` and 
+`brake.right`.
+
+    $(...).panner({ 
+        brake: {
+            left: 'my-left-brake',
+            right : 'my-right-brake'
+        }
+    });
+    
+## Sweep scrolling
+
+`panner` supports sweep scrolling on non-touch devices. All sweep behaviour 
+controlled by `sweep` option.
+
+    $(...).panner({ 
+        sweep : {
+            distance : 150, 
+            timeout : 200,
+            distanceMulti : 3,
+            timeMulti : 10,
+            easing : 'linear'
+        }
+    });
+    
+If user start another scroll while sweeping, sweep scrolling stops.
+    
+### Disabling
+
+Sweep can be turned off exactly as brakes. Just set `sweep` option to `false`.
+
+    $(...).panner({ 
+        sweep : false
+    });
+ 
+### Detection
+
+On start scrolling, `panner` monitors movements. And if distance of last 
+movement exceeds a certain value in the maximum time, `panner` scroll 
+container (`.pan`) in sweep manner.
+
+To control sweep detection, you can use `sweep.distance` and 
+`sweep.timeout` options.
+
+    $(...).panner({ 
+        sweep : {
+            distance : 150, 
+            timeout : 200
+        }
+    });
+
+`sweep.distance` means minimal scroll distance and `sweep.timeout` means 
+maximum time of last movement.
+
+### Distance and speed
+
+`panner` calculates sweep distance and speed calculates on the basis of last 
+scroll distance and time. 
+
+    $(...).panner({ 
+        sweep : {
+            distanceMulti : 3,
+            timeMulti : 10
+        }
+    });
+    
+If `panner` detects sweep, it multiplies scroll distance and time on 
+corresponds options.
+
+### Easing
+
+By default setting of sweep easing is `'linear'`. It is made because of 
+horrible builtin jQuery `'swing'` easing.
+
+    $(...).panner({ 
+        sweep : {
+            easing : 'linear'
+        }
+    });
+
+For other easings, I recommend [jQuery Easing](http://gsgd.co.uk/sandbox/jquery/easing/) plugin. This is drop-in extension 
+for easings. 
+
+    ... plug jquery.easing ...
+    
+    $(...).panner({ 
+        sweep : {
+            easing : 'swing'
+        }
+    });
+
+    ... if you disable jquery.easing, you can see difference.
+    
+## Bonus. Shift on load
+
+On non-touch devices, without scrollbar user may simply not guess that 
+element is scrollable by dragging. To help him, `panner` can slightly shift 
+contents to left and back.
+
+    $(...).panner({ 
+        shift: 0
+    });
+  
+`shift` option is off by default. For shift container contents (`.pan`) on 
+start, use any positive value in pixels.
+
+
+    
---
+++ jquery.panner.0.3.js
@@ -0,0 +1,218 @@
+/**
+ * jquery.panner v0.3
+ * Horisontal panning. With touch fallback. 
+ * Dependencies: jQuery, jQuery.easing (optional)
+ */
+
+(function($) {
+
+    // Namespaces
+    var ns = 'panner';
+    var eventNs = '.' + ns;
+    
+    function Panner(el, options) {
+        this.options = options;
+        this.el = $(el);
+        
+        if (!!!options.brake) {
+            this.brake = false;
+        } else {
+            this.brake = !!options.brake.what ? 
+                    options.brake.what : this.el.parent();
+        }
+        
+        this._lastX = 0;
+        this.init();
+    }
+    
+    /**
+     * Init
+     */
+    Panner.prototype.init = function() {
+        var that = this;
+        
+        this.el.
+            // Set styles for element
+            css(this.options.css)
+        
+            // and bind events 
+            .on('mousedown' + eventNs, function(ev) { 
+                that.startDrag(ev);
+                return false;
+            });
+        this.doShift();
+    };
+    
+    /**
+     * Start dragging.
+     */
+    Panner.prototype.startDrag = function(ev) {
+        // stop all animations and reset sweep.
+        this.el.stop(true);
+        
+        if (!!this.options.sweep) {
+            this.sweepTimer = (new Date()).getTime() ;
+            this.sweepValue = 0; 
+        }
+        
+        this._lastX = ev.pageX;
+        
+        var that = this;
+        this.el
+            // On mouseup need to check sweep status
+            .on('mouseup' + eventNs, function(ev) {
+                    that.stopDrag(); 
+                    that.doSweep(ev);
+            })
+            // On mouseleave just stop dragging
+            .on('mouseleave' + eventNs, function() {
+                    that.stopDrag();
+            })
+            // On mousemove do draggind
+            .on('mousemove' + eventNs, function(ev) {
+                    that.doDrag(ev);
+                    return false;
+            });
+    }
+    
+    /**
+     * Stop dragging
+     */
+    Panner.prototype.stopDrag = function() {
+        this.el.off(
+                'mousemove' + eventNs +
+                ' mouseup' + eventNs +
+                ' mouseleave' + eventNs
+                );
+    }
+    
+    /**
+     * Do drag and maintain sweep state.
+     */
+    Panner.prototype.doDrag = function(ev) {
+        var el = this.el;
+        var x = ev.pageX;
+        var delta = this._lastX - x;
+        var lastScroll = el.scrollLeft();
+        this._lastX = x;
+        
+        el.scrollLeft(lastScroll + delta);
+        
+        // Maintain sweep state if needed
+        if (!!this.options.sweep) {
+            var scrollDelta = el.scrollLeft() - lastScroll;
+            var newSweep = this.sweepValue + scrollDelta;
+            if (Math.abs(this.sweepValue) < Math.abs(newSweep)) {
+                this.sweepValue = newSweep;
+            } else {
+                this.sweepTimer = (new Date()).getTime();
+                this.sweepValue = scrollDelta;
+            }
+        }
+        
+        this.doBrake(lastScroll, delta, ev);
+    }
+    
+    /**
+     * Do brakes if needed.
+     */
+    Panner.prototype.doBrake = function(lastScroll, delta, ev) {
+        if (!!this.brake 
+                && lastScroll == this.el.scrollLeft()
+                && delta != 0) {
+            var brake = this.brake;
+            var opts = this.options.brake;
+            
+            var brakeClass = (delta < 0) ? opts.left : opts.right;
+            brake.addClass(brakeClass);
+            setTimeout(function() {
+                brake.removeClass(brakeClass);
+            }, opts.timeout);
+        }
+    }
+    
+    /**
+     * Do sweep if needed.
+     */
+    Panner.prototype.doSweep = function(ev) {
+        if (!!this.options.sweep) {
+            var sweepTime = (new Date()).getTime() - this.sweepTimer;
+            var opts = this.options.sweep;
+            
+            if (Math.abs(this.sweepValue) >= opts.distance 
+                    && sweepTime < opts.timeout) {
+                var sweepDistance = (this.sweepValue * opts.distanceMulti);
+                var sweepTime = (opts.timeout - sweepTime) * opts.timeMulti;
+                this.el.animate(
+                        {scrollLeft: this.el.scrollLeft() + sweepDistance}, 
+                        sweepTime,
+                        opts.easing);
+            }
+        }
+    }
+    
+    /**
+     * Pull on init if needed.
+     */
+    Panner.prototype.doShift = function() {
+        if (!!this.options.shift) {
+            var opts =  this.options.sweep;
+            var el = this.el;
+            el.animate(
+                    {scrollLeft: this.options.shift},
+                    opts.timeMulti * 10,
+                    opts.easing, 
+                    function() {
+                        el.animate(
+                                {scrollLeft: 0},
+                                opts.timeMulti * 20,
+                                opts.easing);
+                    });
+        }
+    }
+    
+    /**
+     * Entry point.
+     */
+    $.fn[ns] = function(options) {
+        var settings = {
+                
+                // Touch detection
+                touch : function() {
+                    return 'ontouchstart' in document.documentElement;
+                },
+                
+                // Css override for band.
+                css : {'overflow': 'hidden'},
+                
+                // Brake settings
+                brake: {
+                    left : 'brake-left',    // Left brake class
+                    right : 'brake-right',  // Right brake class
+                    timeout : 500               // Brake remove timeout
+                },
+                
+                // Sweep settings
+                sweep : {
+                    distance : 150,     // Minimal distance for sweep detection 
+                    timeout : 200,      // Maximum timeout for sweep detection in millis
+                    distanceMulti : 3,  // Sweep distance multi
+                    timeMulti : 10,     // Sweep time multi
+                    easing : 'linear'   // Sweep easing. Linear by default.
+                        // Highly recommends to use `swing` from 
+                        // jquery.easing. Default `swing` is horrible.
+                },
+                
+                shift: 0                 // Pull on start
+        }
+        
+        options = !!options ? options : {};
+        options = $.extend(true, settings, options);
+        
+        return this.each(function () {
+            if (!!!options.touch() && !$.data(this, 'plugin__' + ns))
+                $.data(this, 'plugin__' + ns, new Panner(this, options));
+        });
+    };
+    
+})(jQuery);
